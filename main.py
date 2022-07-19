08b5572e932973aa17498c99caa1c0d39e1f0c1d
ugrasK <ugurkaraer1989@gmail.com>
2022-07-19 22:37:36 +0300

    created Paddle class to refactor code and used it for creating another paddle with inputs.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import turtle
-from turtle import Screen, Turtle
+from turtle import Screen
+from paddle import Paddle
 
 screen = Screen()
 screen.setup(800, 600)
@@ -7,37 +7,18 @@ screen.bgcolor("black")
 screen.title("Pong")
 screen.tracer(0)
 
-paddle = Turtle("square")
-paddle.color("white")
-paddle.shapesize(stretch_len=1, stretch_wid=5)
-paddle.penup()
-paddle.goto(350, 0)
-
-
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(350, new_y)
-
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(350, new_y)
-
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
 screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
     screen.update()
-
-
-
-
-
-
-
-
 
 
 screen.exitonclick()
