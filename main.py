a97f805f37a226d2d71ef4f639b5581cd5eef821
UgrasK <ugurkaraer1989@gmail.com>
2022-07-19 17:49:14 +0300

    created paddle and added go_up & go_down methods to give user control.

--- main.py
+++ main.py
@@ -1,9 +1,36 @@
-from turtle import Screen
+import turtle
+from turtle import Screen, Turtle
 
 screen = Screen()
 screen.setup(800, 600)
 screen.bgcolor("black")
 screen.title("Pong")
+screen.tracer(0)
+
+paddle = Turtle("square")
+paddle.color("white")
+paddle.shapesize(stretch_len=1, stretch_wid=5)
+paddle.penup()
+paddle.goto(350, 0)
+
+
+def go_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(350, new_y)
+
+def go_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(350, new_y)
+
+
+screen.listen()
+screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+
 
 
 
